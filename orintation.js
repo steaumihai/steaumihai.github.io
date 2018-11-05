04218371ad1e02c448b0e605121020eab4493147
steaumihai <43954436+steaumihai@users.noreply.github.com>
2018-11-05 10:47:07 +0200

    Update orintation.js

--- orintation.js
+++ orintation.js
@@ -1,4 +1,4 @@
-document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.11.05.0";
+document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.11.05.2";
 
 window.addEventListener("deviceorientation", on_device_orientation);
 window.addEventListener("devicemotion", on_device_motion);
@@ -26,7 +26,15 @@ function on_device_orientation(e)
 //------------------------------------------------------
 function on_device_motion(e)
 {
-	document.getElementById("id_acc_z").innerHTML = e.accelerationIncludingGravity.z;
-	document.getElementById("id_acc_x").innerHTML = e.accelerationIncludingGravity.x;
-	document.getElementById("id_acc_y").innerHTML = e.accelerationIncludingGravity.y;
+	var acc_z = e.accelerationIncludingGravity.z;
+	var acc_x = e.accelerationIncludingGravity.x;
+	var acc_y = e.accelerationIncludingGravity.y;
+	
+	document.getElementById("id_acc_z").innerHTML = Math.round(acc_z * 100) / 100;
+	document.getElementById("id_acc_x").innerHTML = Math.round(acc_x * 100) / 100 ;
+	document.getElementById("id_acc_y").innerHTML = Math.round(acc_y * 100) / 100;
+	
+	document.getElementById("id_rot_x").innerHTML = Math.atan(acc_x / acc_z);
+	document.getElementById("id_rot_y").innerHTML = Math.atan(acc_y / acc_z);
+	
 }
