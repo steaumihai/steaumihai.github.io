2aa7d1244436e10a6cc22d938d42fafed550bffd
steaumihai <43954436+steaumihai@users.noreply.github.com>
2018-11-12 11:36:25 +0200

    Update cam.js

--- cam.js
+++ cam.js
@@ -1,5 +1,8 @@
-document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.11.12.2";
+document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.11.12.3";
 document.getElementById("id_button").addEventListener("click",start);
+document.getElementById("id_video").addEventListener("touchstart",on_touch);
+document.getElementById("id_video").addEventListener("mousedown",on_touch);
+
 
 function on_success(e)
 {
@@ -22,3 +25,10 @@ function start()
 	
 }
 //--------------------------
+function on_touch(e)
+{
+	var canvas= document.getElementById("id_canvas");
+	var context= canvas.getContext("2d");
+	var video= document.getElementById("id_video");
+	context.drowImage(video,0,0);
+}
