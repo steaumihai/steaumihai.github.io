53e97d2aa6dbee81ce7fe682a065ca53d185446d
steaumihai <43954436+steaumihai@users.noreply.github.com>
2018-11-12 11:19:55 +0200

    Update cam.js

--- cam.js
+++ cam.js
@@ -1,4 +1,4 @@
-document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.11.12.1";
+document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.11.12.2";
 document.getElementById("id_button").addEventListener("click",start);
 
 function on_success(e)
@@ -18,6 +18,7 @@ function start()
 {
 	var c= {audio:true, video:true}
 	
+	var c ={audio: true, video: {facingMode: "environment"}};
 	navigator.mediaDevices.getUserMedia(c).then(on_success).catch(on_failure);
 	
 }
