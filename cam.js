1057678aaa67e5b233c57451f7a0acb2cdd11485
steaumihai <43954436+steaumihai@users.noreply.github.com>
2018-11-12 11:10:14 +0200

    Add files via upload

---
+++ cam.js
@@ -0,0 +1,24 @@
+document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.11.12.1;
+document.getElementById("id_button").addEventListener("click",start);
+
+function on_success(e)
+{
+	document.getElementById("id_video").srcObject= e;
+	
+}
+//--------------------------
+function on_failure(e)
+{
+	alert("Eroare conectare");
+}
+
+
+//--------------------------
+function start()
+{
+	var c= {audio:true, video:true}
+	
+	navigator.mediaDevices.getUserMedia().then(on_success).catch(on_failure);
+	
+}
+//--------------------------
