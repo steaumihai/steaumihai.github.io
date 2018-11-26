899cc2d20e9119c00ec471810e959e4aec0e535b
steaumihai <43954436+steaumihai@users.noreply.github.com>
2018-11-26 15:24:19 +0200

    Update touch.js

--- touch.js
+++ touch.js
@@ -1,7 +1,8 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.11.26.7";
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.11.26.8";
 var canvas = document.getElementById("id_canvas");
 canvas.addEventListener("touchstart", on_touch_start);
 canvas.addEventListener("touchmove", on_touch_move);
+canvas.addEventListener("touchend", on_touch_end);
 var canvas_bounding_rect = canvas.getBoundingClientRect();
 
 
@@ -56,3 +57,16 @@ function on_touch_move(e)
 	}
 	
 }
+//--------------------------------
+function on_touch_end(e)
+{
+	for (var i = 0; i < e.changedTouches.length; i++){
+		var j =0;
+	for (;j<last_pos_array.length;j++)
+		if (last_pos_array[j].id == e.changedTouches[i].identifier)
+			break;
+		last_pos_array.splice(j,1);
+		
+	}
+
+
