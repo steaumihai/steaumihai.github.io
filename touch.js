e0a4fad4870e0dbe3a1a977947a78afb76fe09de
steaumihai <43954436+steaumihai@users.noreply.github.com>
2018-11-26 15:14:35 +0200

    updade

--- touch.js
+++ touch.js
@@ -4,7 +4,8 @@ canvas.addEventListener("touchstart", on_touch_start);
 canvas.addEventListener("touchmove", on_touch_move);
 var canvas_bounding_rect = canvas.getBoundingClientRect();
 
-var last_pos = {x: 0, y: 0};
+
+var last_pos_array = [];
 //------------------------------------
 function on_touch_start(e)
 {
@@ -16,8 +17,12 @@ function on_touch_start(e)
 					10,
 					0, 2 * Math.PI);
 		context.stroke();
-		last_pos.x = e.changedTouches[i].pageX;
-		last_pos.y = e.changedTouches[i].pageY;
+		var last_pos = {x:last_pos.x = e.changedTouches[i].pageX,
+					    y:last_pos.y = e.changedTouches[i].pageY,
+						id: e.changedTouches[i].identifier};
+		last_pos_array.push(last_pos);
+		
+		
 	}
 }
 //------------------------------------
@@ -26,10 +31,15 @@ function on_touch_move(e)
 	e.preventDefault();
 	
 	for (var i = 0; i < e.changedTouches.length; i++){
+		var j =0;
+	for (;j<last_pos_array.length;j++)
+		if (last_pos_array[j].id == e.changedTouches[i].identifier)
+			break;
+		
 		var context = canvas.getContext("2d");
 		context.beginPath();
 		context.lineWidth =20;
-		context.moveTo(last_pos.x - canvas_bounding_rect.left, last_pos.y - canvas_bounding_rect.top);
+		context.moveTo(last_pos_array[j].x - canvas_bounding_rect.left, last_pos_array[j].y - canvas_bounding_rect.top);
 		context.lineTo(e.changedTouches[i].pageX - canvas_bounding_rect.left,
 					e.changedTouches[i].pageY - canvas_bounding_rect.top);
 		
@@ -41,8 +51,8 @@ function on_touch_move(e)
 					10,
 					0, 2 * Math.PI);
 		context.stroke();
-		last_pos.x = e.changedTouches[i].pageX;
-		last_pos.y = e.changedTouches[i].pageY;		
+		last_pos_array[j].x = e.changedTouches[i].pageX;
+		last_pos_array[j].y = e.changedTouches[i].pageY;		
 	}
 	
 }
