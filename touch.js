ba90b4b2ce8fd4fdb020562a06d484916dc783b4
steaumihai <43954436+steaumihai@users.noreply.github.com>
2018-11-26 14:57:39 +0200

    Update touch.js

--- touch.js
+++ touch.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.11.26.6";
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.11.26.7";
 var canvas = document.getElementById("id_canvas");
 canvas.addEventListener("touchstart", on_touch_start);
 canvas.addEventListener("touchmove", on_touch_move);
